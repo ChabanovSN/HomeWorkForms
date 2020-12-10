9617c679918549b8988398b193d3f72834209ffe
Sergey Chabanov <chabanovsn@mail.ru>
2020-12-10 23:02:37 +0300

    first hw done

---
+++ HomeWorkForms/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Forms;
+namespace HomeWorkForms
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Application.Run(new Form1());
+        }
+    }
+    public class Form1 : Form
+    {
+        private int leftBtn, midBtn,rightBtn;
+
+        public Form1() {
+            Width = 600;
+            Click += Control1_MouseClick;
+            ShowCount();
+           }
+        private void Control1_MouseClick(Object sender,EventArgs e)
+        {
+
+            if( e is MouseEventArgs arg) {
+                if (arg.Button == MouseButtons.Left)
+                    leftBtn++;
+               if (arg.Button == MouseButtons.Right)
+                    rightBtn++;
+                if (arg.Button == MouseButtons.Middle)
+                    midBtn++;
+
+                ShowCount();
+            }
+        }
+        private void ShowCount() {
+            Text = $"левая кнопка: {leftBtn}, средняя{midBtn}, правая {rightBtn}";
+        }
+
+
+    }
+}
